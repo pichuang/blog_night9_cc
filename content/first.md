3b75dba2196f990326af67f9368420fb6ed68b0a
Roan Huang <pichuang@cs.nctu.edu.tw>
2015-02-10 18:08:23 +0800

    blog.night9.cc pelican enviroment init

---
+++ content/first.md
@@ -0,0 +1,9 @@
+Title: First page
+Date: 2015-02-10 14:00
+Category: Misc
+Tags: misc 
+Slug: first
+Author: Roan Huang
+Summary: Simply test
+
+pichuang_gg_1231231231232
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'pichuang'
+SITENAME = u'Roan'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Taipei'
+
+THEME = 'pelican-bootstrap3' 
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Social widget
+SOCIAL = (('Faceb00k', 'http://wwww.facebook.com/paulintoro'),
+          ('Github', 'https://github.com/pichuang/'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
